9f12f763666fe02e739f091086db716109e5dd4b
liuruheng <liuruheng@seuic.com>
2017-09-30 16:51:35 +0800

    新浪贵金属报价JS源获取方式

--- WebApi-20170928/sina-finance-futures.py
+++ WebApi-20170928/sina-finance-futures.py
@@ -1,4 +1,4 @@
-#coding=utf-8
+# -*- coding:utf-8 -*-
 
 import requests
 from time import ctime
@@ -10,7 +10,16 @@ import re
 XAU_URL='http://finance.sina.com.cn/futures/quotes/XAU.shtml'
 #伦敦银
 XAG_URL='http://finance.sina.com.cn/futures/quotes/XAG.shtml'
-
+#XAU-JS
+XAG_JS_URL = 'http://hq.sinajs.cn/?_=1506753075305/&list=hf_XAU'
+#XAG-JS
+XAG_JS_URL = 'http://hq.sinajs.cn/?_=1506752449226/&list=hf_XAG'
+#JS_Dict
+JS_DICT = ['real-price','wave-rate','buy','sell','max-price','min-price',
+           'trade-end-time','yestoday-price','close-price',
+           'amount','volume','buy-amount','sell-amount']
+#JS_DICT = ['最新价','涨跌率','买价','卖价','最高价','最低价',
+#           '收盘时间','昨结算','收盘价','持仓量','成交量','买入量','卖出量']
 def getXAGHTMLText(url):
     """获取伦敦银HTML页面"""
     try:
@@ -46,7 +55,26 @@ def getXAGInfo(url):
     #except:
     #    print('有异常产生')
 
+def getXAGJS():
+    '''从JS中获取现货白银报价等相关信息'''
+    r = requests.get(XAG_JS_URL)
+    print("Status code:", r.status_code)
+
+    u = urlopen(XAG_JS_URL)
+
+    js_content = (u.read().split('='))[1].split(',')
+    js_dict = dict(zip(JS_DICT,js_content))
+
+    print js_dict.keys()
+
+    for i in js_dict:
+        print js_dict[i]
+
+    print js_dict.items()
+    print list(js_dict.iteritems())
+
 def main():
-    getXAGInfo(XAG_URL)
+    #getXAGInfo(XAG_URL)
+    getXAGJS()
 
 main()
